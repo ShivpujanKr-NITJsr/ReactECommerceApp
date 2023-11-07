e2684759f7a004e96e8f456a10c791c240b1e0ef
ShivpujanKr-NITJsr <sahil.8228053857@gmail.com>
2023-11-07 18:29:51 +0530

    added spinner over the button for showing as it is loading and fetching movies from api

--- src/Components/AppComponents.js
+++ src/Components/AppComponents.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react"
-import { Container, Row } from "react-bootstrap"
+import { Container, Row, Spinner } from "react-bootstrap"
 import SingleProduct from "./SingleProduct"
 import { productsArr } from "../ProductsArr/productsArr"
 
@@ -11,7 +11,10 @@ const AppComponents =(props)=>{
 
   const [movies,setMovies]=useState([])
 
+  const [loading,setLoad] =useState(false)
+
   async function fetchMoviesHandler(){
+    setLoad(true);
     let response=await fetch('https://swapi.dev/api/films/')
     let data=await response.json()
     
@@ -25,13 +28,21 @@ const AppComponents =(props)=>{
           releaseDate: movieData.release_date
         }
       })
+
+      setLoad(false)
       setMovies(data.results)
     }
   
 
     return <Container className='d-flex  align-items-center justify-content-center'>
+       
     <Row className='d-flex gap-5 align-items-center justify-content-center mt-5'>
-      <button onClick={fetchMoviesHandler} >get Movies</button>
+      
+      <button onClick={fetchMoviesHandler} disabled={loading} >{loading? <Spinner animation="border" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </Spinner> : 'Get'} {loading? 'loading... Movies': 'Movies'}</button>
+      
+     
       {productsArr.map(item => {
         return <SingleProduct item={item} key={Math.random().toString()+'12'}/>
       })}
